36bba8371ace603e0429d05efdc9b551ab67a05d
Miguel Soto <qk_leugim96@hotmail.com>
2023-03-16 21:24:12 -0600

    Organización de carpetas y experimentos añadidos

--- Doctorado/Scripts/baseline_model.py
+++ Doctorado/Scripts/baseline_model.py
@@ -5,7 +5,8 @@ from sklearn import metrics
 
 # Main class
 class Baseline:
-    def __init__(self, train_data, test_data, x_label_column, y_label_column, target_names):
+    def __init__(self, model, train_data, test_data, x_label_column, y_label_column, target_names):
+        self.model = model
         self.train_data = train_data
         self.test_data = test_data
         self.x_label_column = x_label_column
@@ -13,14 +14,14 @@ class Baseline:
         self.target_names = target_names
 
     
-    def baseline(self, model, train_data, test_data, x_label_column, y_label_column, target_names):
+    def baseline(self):
         # Training data
-        x_train = train_data[x_label_column].tolist()
-        y_train = train_data[y_label_column].tolist()
+        x_train = self.train_data[self.x_label_column].tolist()
+        y_train = self.train_data[self.y_label_column].tolist()
 
         # Test data
-        x_test = test_data[x_label_column].tolist()
-        y_test = test_data[y_label_column].tolist()
+        x_test = self.test_data[self.x_label_column].tolist()
+        y_test = self.test_data[self.y_label_column].tolist()
 
         # Vectorizing text data
         vectorizer = TfidfVectorizer(ngram_range=(1, 3), min_df=3)
@@ -28,12 +29,15 @@ class Baseline:
         x_test = vectorizer.transform(x_test)
 
         # Fitting model
-        print(f'Fitting {y_label_column} model')
-        model.fit(x_train, y_train)
-        y_pred = model.predict(x_test)
+        print(f'Fitting {self.y_label_column} model')
+        final_model = self.model
+        final_model.fit(x_train, y_train)
+        y_pred = final_model.predict(x_test)
 
         # Results
-        print(metrics.classification_report(y_test, y_pred, target_names=target_names))
+        print(metrics.classification_report(y_test, 
+                                            y_pred, 
+                                            target_names=self.target_names))
 
 
-        return model, vectorizer
+        return final_model, vectorizer
