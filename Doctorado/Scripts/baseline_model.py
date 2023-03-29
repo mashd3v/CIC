5f29011ce054e1eec149defeed16d083840f3778
Miguel Soto <qk_leugim96@hotmail.com>
2023-03-29 15:18:33 -0600

    PAN20 Fake news

--- Doctorado/Scripts/baseline_model.py
+++ Doctorado/Scripts/baseline_model.py
@@ -7,12 +7,16 @@ from sklearn import metrics
 
 # Main class
 class Baseline:
-    def __init__(self, model, train_data, test_data, x_label_column, y_label_column, target_names):
+    def __init__(self, model, train_data, test_data, x_label_column, y_label_column, 
+                 ngram_range, min_df, model_name, target_names):
         self.model = model
         self.train_data = train_data
         self.test_data = test_data
         self.x_label_column = x_label_column
         self.y_label_column = y_label_column
+        self.ngram_range = ngram_range
+        self.min_df = min_df
+        self.model_name = model_name
         self.target_names = target_names
 
     
@@ -26,12 +30,12 @@ class Baseline:
         y_test = self.test_data[self.y_label_column].tolist()
 
         # Vectorizing text data
-        vectorizer = TfidfVectorizer(ngram_range=(1, 3), min_df=3)
+        vectorizer = TfidfVectorizer(ngram_range=self.ngram_range, 
+                                     min_df=self.min_df)
         x_train = vectorizer.fit_transform(x_train)
         x_test = vectorizer.transform(x_test)
 
         # Fitting model
-        print(f'Fitting {self.y_label_column} model')
         final_model = self.model
         final_model.fit(x_train, y_train)
         y_pred = final_model.predict(x_test)
@@ -42,8 +46,8 @@ class Baseline:
                                             target_names=self.target_names)
         confusion_matrix = metrics.confusion_matrix(y_test, y_pred)
         accuracy = metrics.accuracy_score(y_test, y_pred)
-        
-        print(clf_report)
+
+        print(f'Model {self.model_name} ({self.y_label_column}-{self.ngram_range}-{self.min_df}) --> Accuracy: {round(accuracy, 2)}')
         
         return final_model, vectorizer, [clf_report, confusion_matrix, accuracy]
 
